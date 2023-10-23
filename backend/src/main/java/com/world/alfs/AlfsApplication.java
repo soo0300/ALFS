d5cafad4f7d7170d3fbbca0d04665a8875a09aee
연주원 <joo1yeon@naver.com>
2023-10-23 10:19:07 +0900

    :sparkles: feat: 백엔드 초기설정 1

---
+++ backend/src/main/java/com/world/alfs/AlfsApplication.java
@@ -0,0 +1,13 @@
+package com.world.alfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfsApplication.class, args);
+	}
+
+}
