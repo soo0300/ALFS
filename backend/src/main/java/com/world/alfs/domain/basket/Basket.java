4eb08de96754b156be22d807650efb9e450b21be
김수진 <soo0300@naver.com>
2023-10-25 09:08:14 +0900

    ✨feat: 도메인 추가

--- backend/src/main/java/com/world/alfs/domain/basket/Basket.java
+++ backend/src/main/java/com/world/alfs/domain/basket/Basket.java
@@ -1,18 +1,16 @@
-package com.world.alfs.domain.board;
+package com.world.alfs.domain.basket;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Board{
+public class Basket {
     @Id
     private Long id;
-
 }
