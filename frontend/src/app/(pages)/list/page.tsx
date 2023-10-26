6248004739c3db6391c9641abfebdb41673020ea
홍주영 <darkssg@naver.com>
2023-10-26 15:53:47 +0900

    ✨feat: 리스트 페이지 디테일페이지 연결 및 디자인 수정

--- frontend/src/app/(pages)/list/page.tsx
+++ frontend/src/app/(pages)/list/page.tsx
@@ -1,23 +1,31 @@
 import React from "react";
 import ListData from "../../_asset/data/item_List.json";
 import Card from "@/app/_components/card/Card";
-type Props = {};
+import Link from "next/link";
+type IdProps = { id: number };
 
-export default function Page({}: Props) {
+export default function Page({ id }: IdProps) {
   return (
     <div className="Container w-[800px] h-auto mt-[124px] ml-[418px]">
-      <div className="grid grid-cols-3 justify-center">
+      총 {ListData.length}건
+      <>
+        <span className="flex justify-end">가격높은순</span>
+      </>
+      <hr />
+      <div className="grid grid-cols-3 mx-auto mt-[10px]">
         {ListData.map((item: any) => (
-          <div key={item.id} className="w-[178px] h-[450px]">
-            <Card
-              name={item.name}
-              image={item.images}
-              id={item.id}
-              title={item.title}
-              price={item.price}
-              sale={item.sale}
-              delivery={item.delivery}
-            />
+          <div key={item.id} className="w-[178px] h-[450px] ml-[44px]">
+            <Link href={{ pathname: `/detail/${item.id}`, query: { id: id } }}>
+              <Card
+                name={item.name}
+                image={item.images}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                sale={item.sale}
+                delivery={item.delivery}
+              />
+            </Link>
           </div>
         ))}
       </div>
