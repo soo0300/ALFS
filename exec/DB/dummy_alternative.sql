4b1fa543a38ecdc60705723acf3ef29411d5ed1b
연주원 <joo1yeon@naver.com>
2023-10-26 14:47:20 +0900

    ✨feat: 대체 식품 더미 데이터 추가

---
+++ exec/DB/dummy_alternative.sql
@@ -0,0 +1,65 @@
+# 난류
+insert into alternative(alternative_name, parent_id) values ('생선', 9);
+insert into alternative(alternative_name, parent_id) values ('쇠고기', 9);
+insert into alternative(alternative_name, parent_id) values ('돼지고기', 9);
+insert into alternative(alternative_name, parent_id) values ('두부', 9);
+
+# 우유
+insert into alternative(alternative_name, parent_id) values ('두유', 10);
+
+# 메밀
+insert into alternative(alternative_name, parent_id) values ('쌀', 2);
+insert into alternative(alternative_name, parent_id) values ('감자', 2);
+
+# 땅콩
+insert into alternative(alternative_name, parent_id) values ('깨', 5);
+
+# 대두
+insert into alternative(alternative_name, parent_id) values ('계란', 3);
+insert into alternative(alternative_name, parent_id) values ('우유', 3);
+insert into alternative(alternative_name, parent_id) values ('소금', 3);
+insert into alternative(alternative_name, parent_id) values ('포도씨기름', 3);
+
+# 밀
+insert into alternative(alternative_name, parent_id) values ('쌀', 1);
+insert into alternative(alternative_name, parent_id) values ('감자', 1);
+
+# 생선(고등어)
+insert into alternative(alternative_name, parent_id) values ('쇠고기', 14);
+insert into alternative(alternative_name, parent_id) values ('닭고기', 14);
+insert into alternative(alternative_name, parent_id) values ('돼지고기', 14);
+insert into alternative(alternative_name, parent_id) values ('두부', 14);
+insert into alternative(alternative_name, parent_id) values ('계란', 14);
+
+# 새우
+insert into alternative(alternative_name, parent_id) values ('쇠고기', 13);
+insert into alternative(alternative_name, parent_id) values ('닭고기', 13);
+insert into alternative(alternative_name, parent_id) values ('돼지고기', 13);
+insert into alternative(alternative_name, parent_id) values ('두부', 13);
+insert into alternative(alternative_name, parent_id) values ('계란', 13);
+
+# 게
+insert into alternative(alternative_name, parent_id) values ('쇠고기', 19);
+insert into alternative(alternative_name, parent_id) values ('닭고기', 19);
+insert into alternative(alternative_name, parent_id) values ('돼지고기', 19);
+insert into alternative(alternative_name, parent_id) values ('두부', 19);
+insert into alternative(alternative_name, parent_id) values ('계란', 19);
+
+# 돼지고기
+insert into alternative(alternative_name, parent_id) values ('쇠고기', 8);
+insert into alternative(alternative_name, parent_id) values ('닭고기', 8);
+insert into alternative(alternative_name, parent_id) values ('생선', 8);
+insert into alternative(alternative_name, parent_id) values ('두부', 8);
+insert into alternative(alternative_name, parent_id) values ('계란', 8);
+
+# 복숭아
+insert into alternative(alternative_name, parent_id) values ('수박', 6);
+insert into alternative(alternative_name, parent_id) values ('참외', 6);
+
+# 토마토
+insert into alternative(alternative_name, parent_id) values ('소금', 7);
+
+
+
+
+
