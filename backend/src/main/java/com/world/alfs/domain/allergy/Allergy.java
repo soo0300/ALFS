a210b6e4a1986667c61273f8538a5e2eb25e1f95
연주원 <joo1yeon@naver.com>
2023-10-26 14:10:23 +0900

    ✨feat: 22가지 알러지 더미데이터 추가

--- backend/src/main/java/com/world/alfs/domain/allergy/Allergy.java
+++ backend/src/main/java/com/world/alfs/domain/allergy/Allergy.java
@@ -4,13 +4,19 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Allergy {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String allergyName;
+
+    @Column(nullable = false)
+    private int allergyType;
 }
