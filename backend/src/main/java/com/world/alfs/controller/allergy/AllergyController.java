0e90fb84339ad3f760ae8e3cc946b8a480abbd77
연주원 <joo1yeon@naver.com>
2023-10-27 15:46:53 +0900

    ✨feat: 22가지 알러지 유발 식품 리스트

--- backend/src/main/java/com/world/alfs/controller/allergy/AllergyController.java
+++ backend/src/main/java/com/world/alfs/controller/allergy/AllergyController.java
@@ -1,10 +1,15 @@
 package com.world.alfs.controller.allergy;
 
+import com.world.alfs.controller.ApiResponse;
+import com.world.alfs.controller.allergy.response.AllergyResponse;
 import com.world.alfs.service.allergy.AllergyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AllergyController {
 
     private final AllergyService allergyService;
+
+    @GetMapping("/list")
+    public ApiResponse<List<AllergyResponse>> getAllergyList() {
+        List<AllergyResponse> response = allergyService.getAllergyList();
+        return ApiResponse.ok(response);
+    }
 
 }
