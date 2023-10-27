74710049f97c48644eb449f6f0b19be9e5069c27
홍주영 <darkssg@naver.com>
2023-10-28 08:27:30 +0900

    🌈style: 애니메이션 추가 test 및 컴포넌트 생성

---
+++ frontend/src/app/_components/animate/Emoji.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Lottie from "react-lottie-player";
+// Alternatively:
+// import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
+
+import lottieJson from "../../_asset/data/Animation - 1698392084394.json";
+
+export default function Example() {
+  return (
+    <Lottie
+      loop
+      animationData={lottieJson}
+      play
+      // style 은 선택
+      style={{ width: 150, height: 150 }}
+    />
+  );
+}
