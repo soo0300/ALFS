14d147d10d25781ded57a5e881d04647a2e6a113
daehyuk <sdh5645@gmail.com>
2023-10-27 18:01:12 +0900

    feat:캐러셀적용

---
+++ frontend/src/app/(pages)/main/_components/Carousel.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+import Alps from "../../../_asset/img/알프스.jpg";
+
+export default function Carousel() {
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+  return (
+    <div className="min-w-[1000px] flex justify-center">
+      <div className="w-[1000px] ">
+        <Slider {...settings}>
+          <div className="flex justify-center w-[1000px] bg-green-300">
+            <Image src={Alps} alt="asdf"></Image>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,48 @@
-import React from "react";
-import Nav from "./_components/header/Nav";
+"use client";
+
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Image from "next/image";
+import Alps from "../app/_asset/img/알프스.jpg";
 
 type Props = {};
 
-export default function page({}: Props) {
-  return <></>;
+export default function Page({}: Props) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div className="min-w-[1000px] flex justify-center">
+      <div className="w-[1000px]">
+        <div data-aos="fade-up" className="h-[300px] border-[1px]">
+          <Image src={Alps} alt="asdf"></Image>
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+        <div data-aos="fade-up" className="h-[300px]">
+          asdfad
+        </div>
+      </div>
+    </div>
+  );
 }
