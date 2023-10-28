796f1342216a72578c9f0801435c32509ef75c1f
연주원 <joo1yeon@naver.com>
2023-10-28 16:19:56 +0900

    ✨feat: 대체 식품 카테고리 리스트

--- backend/src/main/java/com/world/alfs/service/alternative/AlternativeService.java
+++ backend/src/main/java/com/world/alfs/service/alternative/AlternativeService.java
@@ -1,13 +1,41 @@
 package com.world.alfs.service.alternative;
 
+import com.world.alfs.controller.alternative.response.CategoryResponse;
+import com.world.alfs.domain.alternative.Alternative;
 import com.world.alfs.domain.alternative.repository.AlternativeRepository;
+import com.world.alfs.service.alternative.dto.GetCategoryListDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @Service
 @RequiredArgsConstructor
 public class AlternativeService {
+
     private final AlternativeRepository alternativeRepository;
+
+    // 대체 식품 카테고리 리스트
+    public List<CategoryResponse> getCategoryList(GetCategoryListDto dto) {
+        List<String> list = alternativeRepository.findByParentIdIn(dto.getIdList()).stream()
+                .map(Alternative::getAlternativeName)
+                .distinct()
+                .collect(Collectors.toList());
+
+        List<CategoryResponse> categoryResponseList = new ArrayList<>();
+        for (String s : list) {
+            CategoryResponse response = CategoryResponse.builder()
+                    .alternativeName(s)
+                    .build();
+
+            categoryResponseList.add(response);
+        }
+
+        return categoryResponseList;
+    }
+
 }
