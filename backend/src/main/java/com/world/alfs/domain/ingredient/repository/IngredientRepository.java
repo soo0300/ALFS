fc2d386eafba115ed4ec88bf44e72b2e240143b6
연주원 <joo1yeon@naver.com>
2023-10-30 15:35:46 +0900

    ✨feat: 원재료 repository 구현

--- backend/src/main/java/com/world/alfs/domain/ingredient/repository/IngredientRepository.java
+++ backend/src/main/java/com/world/alfs/domain/ingredient/repository/IngredientRepository.java
@@ -4,6 +4,11 @@ import com.world.alfs.domain.ingredient.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+
+    Optional<Ingredient> findByName(String name);
+
 }
