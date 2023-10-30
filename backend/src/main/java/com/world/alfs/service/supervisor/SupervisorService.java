97bfac4bb0a7e22670f68628a21ebaff4f9252ea
손효민 <sondoh100@naver.com>
2023-10-30 19:41:29 +0900

    ✨feat: file 형태 OCR 원재료 추출 서비스단 추가

--- backend/src/main/java/com/world/alfs/service/supervisor/SupervisorService.java
+++ backend/src/main/java/com/world/alfs/service/supervisor/SupervisorService.java
@@ -3,6 +3,7 @@ package com.world.alfs.service.supervisor;
 import com.world.alfs.controller.supervisor.response.SupervisorLoginResponse;
 import com.world.alfs.domain.supervisor.Supervisor;
 import com.world.alfs.domain.supervisor.repository.SupervisorRepository;
+import com.world.alfs.service.supervisor.dto.OcrFileDto;
 import com.world.alfs.service.supervisor.dto.OcrUrlDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,9 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -34,6 +33,7 @@ public class SupervisorService {
     private String secretKey;
     @Value("${OCR_INVOKE_URL}")
     private String apiURL;
+    private String boundary;
 
 
     public SupervisorLoginResponse loginSupervisor(String supervisorIdentifier, String supervisorPassword) {
@@ -59,7 +59,7 @@ public class SupervisorService {
         List<String> parseData = null;
         try {
             URL url = new URL(apiURL);
-            HttpURLConnection connection = createRequestHeader(url);
+            HttpURLConnection connection = createRequestHeader(url, false);
             createRequestBody(connection, dto);
 
             StringBuilder response = getResponseData(connection);
@@ -77,16 +77,54 @@ public class SupervisorService {
         return parseData;
     }
 
-    public HttpURLConnection createRequestHeader(URL url) throws IOException {
+
+    public List<String> getFileIngredient(OcrFileDto dto) {
+        log.debug("시크릿키 "+secretKey);
+        log.debug("url "+apiURL);
+
+        List<String> parseData = null;
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection connection = createRequestHeader(url, true);
+            createRequestBody(connection, dto);
+
+            StringBuilder response = getResponseData(connection);
+            log.debug("getFileIngredient 응답 = "+response);
+            parseData = jsonparse(response);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+            e.printStackTrace();
+        }
+
+        log.debug("getFileIngredient 끝");
+
+        return parseData;
+
+    }
+
+    public HttpURLConnection createRequestHeader(URL url, Boolean value) throws IOException {
+
         log.debug("createRequestHeader 시작");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setUseCaches(false);
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
-        connection.setReadTimeout(5000);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json;");
-        connection.setRequestProperty("X-OCR-SECRET", secretKey);
+
+        if(Boolean.FALSE.equals(value)){
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setReadTimeout(30000);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json;");
+            connection.setRequestProperty("X-OCR-SECRET", secretKey);
+        }else{
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setReadTimeout(30000);
+            connection.setRequestMethod("POST");
+            boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            connection.setRequestProperty("X-OCR-SECRET", secretKey);
+        }
 
         log.debug("createRequestHeader 끝");
         return connection;
@@ -117,6 +155,72 @@ public class SupervisorService {
         outputStream.close();
 
         log.debug("createRequestBody 끝");
+    }
+
+
+    public void createRequestBody(HttpURLConnection connection, OcrFileDto dto) throws IOException {
+        log.debug("createRequestBody 시작");
+        JSONObject json = new JSONObject();
+        json.put("version", "v1");
+        json.put("requestId", UUID.randomUUID().toString());
+        json.put("timestamp", System.currentTimeMillis());
+        json.put("lang", "ko");
+        json.put("enableTableDetection", true);
+
+        JSONObject image = new JSONObject();
+        image.put("format", dto.getFormat());
+        image.put("name", dto.getName());
+
+        JSONArray images = new JSONArray();
+        images.put(image);
+        json.put("images", images);
+        String postParams = json.toString();
+
+        connection.connect();
+        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+        File file = multipartFileToFile(dto.getFile());
+        outputStream.write(json.toString().getBytes(StandardCharsets.UTF_8));
+
+        writeMultiPart(outputStream, postParams, file, boundary);
+        outputStream.flush();
+        outputStream.close();
+
+        log.debug("createRequestBody 끝");
+    }
+
+    private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+            IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("--").append(boundary).append("\r\n");
+        sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+        sb.append(jsonMessage);
+        sb.append("\r\n");
+
+        out.write(sb.toString().getBytes("UTF-8"));
+        out.flush();
+
+        if (file != null && file.isFile()) {
+            out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+            StringBuilder fileString = new StringBuilder();
+            fileString
+                    .append("Content-Disposition:form-data; name=\"file\"; filename=");
+            fileString.append("\"" + file.getName() + "\"\r\n");
+            fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+            out.write(fileString.toString().getBytes("UTF-8"));
+            out.flush();
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                byte[] buffer = new byte[8192];
+                int count;
+                while ((count = fis.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                }
+                out.write("\r\n".getBytes());
+            }
+
+            out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+        }
+        out.flush();
     }
 
 
@@ -182,6 +286,12 @@ public class SupervisorService {
         log.debug("jsonparse 끝");
 
         return result;
+    }
+
+    public File multipartFileToFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        multipartFile.transferTo(file);
+        return file;
     }
 
 }
