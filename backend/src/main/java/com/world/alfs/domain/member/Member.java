1aafd37fcd9ec2ab9cf173a1dbe8d65ae24b6143
안종상 <skqlck-@naver.com>
2023-10-30 11:08:32 +0900

    ✨feat: Member Domain

--- backend/src/main/java/com/world/alfs/domain/member/Member.java
+++ backend/src/main/java/com/world/alfs/domain/member/Member.java
@@ -12,6 +12,42 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false)
+    private String identifier;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int birth;
+
+    @Column(nullable = false)
+    private String address_1;
+
+    private String address_2;
+
+    @Column(nullable = false)
+    private int point;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "phone_number",nullable = false)
+    private int phoneNumber;
+
+//    @OneToMany(mappedBy = "member")
+//    private List<Basket> baskets = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<Board> boards = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberAllergy> memberAllergies = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<Wining> winings = new ArrayList<>();
 
 }
--- backend/src/main/java/com/world/alfs/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/world/alfs/domain/member/repository/MemberRepository.java
@@ -4,6 +4,17 @@ import com.world.alfs.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findByIdentifierAndPassword(String identifier, String password);
+
+    Optional<Member> findByIdentifier(String identifier);
+
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByPhoneNumber(int phoneNumber);
+
 }
