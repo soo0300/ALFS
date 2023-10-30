36b98b238d72a4dfa0523a6cde207225211c4430
손효민 <sondoh100@naver.com>
2023-10-30 11:56:22 +0900

    ✨feat: url 형태 OCR 원재료 추출 컨트롤러 추가

--- backend/src/main/java/com/world/alfs/controller/supervisor/SupervisorController.java
+++ backend/src/main/java/com/world/alfs/controller/supervisor/SupervisorController.java
@@ -3,7 +3,6 @@ package com.world.alfs.controller.supervisor;
 import com.world.alfs.controller.ApiResponse;
 import com.world.alfs.controller.supervisor.request.OcrUrlRequest;
 import com.world.alfs.controller.supervisor.request.SupervisorLoginRequest;
-import com.world.alfs.controller.supervisor.response.OcrUrlResponse;
 import com.world.alfs.controller.supervisor.response.SupervisorLoginResponse;
 import com.world.alfs.service.supervisor.SupervisorService;
 import com.world.alfs.service.supervisor.dto.OcrUrlDto;
@@ -13,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,11 +33,13 @@ public class SupervisorController {
     }
 
     @PostMapping("/ocr/url")
-    public ApiResponse<OcrUrlResponse> getUrlIngredient(@RequestBody OcrUrlRequest request){
+    public ApiResponse<List<String>> getUrlIngredient(@RequestBody OcrUrlRequest request){
 
         OcrUrlDto dto = request.toDto();
-        OcrUrlResponse response = supervisorService.getUrlIngredient(dto);
-
+        List<String> response = supervisorService.getUrlIngredient(dto);
+        if(response.isEmpty()){
+            return ApiResponse.badRequest("인식된 원재료명이 없습니다.");
+        }
         return ApiResponse.ok(response);
     }
 }
