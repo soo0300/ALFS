3e02357249eed2e0517afae1811757091620d291
연주원 <joo1yeon@naver.com>
2023-10-30 15:32:31 +0900

    ✨feat: 원재료명 entity 구현

--- backend/src/main/java/com/world/alfs/domain/ingredient/Ingredient.java
+++ backend/src/main/java/com/world/alfs/domain/ingredient/Ingredient.java
@@ -1,9 +1,11 @@
 package com.world.alfs.domain.ingredient;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -11,7 +13,17 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Ingredient {
+
     @Id
     private Long id;
+
+    @Column
+    private String name;
+
+    @Builder
+    public Ingredient(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
 }
