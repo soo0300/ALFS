35a8b5c97e96836dd0d0f5b62d1ae3703dcd903c
daehyuk <sdh5645@gmail.com>
2023-10-30 17:59:38 +0900

    ✨feat: 메인페이지에 lottie추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,27 +5,44 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Image from "next/image";
 import Alps from "../app/_asset/img/알프스.jpg";
+import Emogi from "../app/_components/animate/Emoji";
+import Food from "./_components/animate/food";
 
-type Props = {};
-
-export default function Page({}: Props) {
+export default function Page() {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
     <div className="min-w-[1000px] flex justify-center">
-      <div className="w-[1000px] flex flex-col">
-        <div data-aos="fade-up" className=" w-[500px] h-[300px] border-[1px] flex justify-center items-center">
-          <p className="text-[30px]">
-            새로운 기능!
-            <br />
-            알러지 기능을 확인해보세요{" "}
-          </p>
-          {/* <Image src={Alps} alt="asdf"></Image> */}
+      <div className="flex flex-col">
+        <div className="w-[1000px] flex">
+          <div data-aos="fade-up" className=" w-[500px] h-[300px]  flex justify-center items-center">
+            <p className="text-[30px]">
+              새로운 기능!
+              <br />
+              알러지 기능을 확인해보세요!
+            </p>
+          </div>
+          <div data-aos="fade-up" className=" w-[500px] h-[300px] flex justify-center items-center">
+            <Food />
+          </div>
         </div>
-        <div data-aos="fade-up" className=" w-[500px] h-[300px] border-[1px]">
-          asdf
-          {/* <Image src={Alps} alt="asdf"></Image> */}
+
+        <div className="w-[1000px] flex"></div>
+
+        <div className="w-[1000px] flex">
+          <div data-aos="fade-up" className=" w-[500px] h-[300px] border-[1px] flex justify-center items-center">
+            <p className="text-[30px]">
+              새로운 기능!
+              <br />
+              알러지 기능을 확인해보세요{" "}
+            </p>
+            {/* <Image src={Alps} alt="asdf"></Image> */}
+          </div>
+          <div data-aos="fade-up" className=" w-[500px] h-[300px] border-[1px]">
+            asdf
+            {/* <Image src={Alps} alt="asdf"></Image> */}
+          </div>
         </div>
       </div>
     </div>
