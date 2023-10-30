c86dc131e070afe9079c5e1e0b57fd0680e1f410
김수진 <soo0300@naver.com>
2023-10-30 14:22:47 +0900

    ✨feat: 회원 알러지 컨트롤러 & DTO 작성

---
+++ backend/src/main/java/com/world/alfs/service/member_allergy/dto/AddMemberAllergyDto.java
@@ -0,0 +1,34 @@
+package com.world.alfs.service.member_allergy.dto;
+
+import com.world.alfs.domain.allergy.Allergy;
+import com.world.alfs.domain.member.Member;
+import com.world.alfs.domain.member_allergy.MemberAllergy;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class AddMemberAllergyDto {
+    private Long id;
+
+    private Long member_id;
+
+    private Long allergy_id;
+
+    @Builder
+    public AddMemberAllergyDto(Long id, Long member_id, Long allergy_id) {
+        this.id = id;
+        this.member_id = member_id;
+        this.allergy_id = allergy_id;
+    }
+
+    public MemberAllergy toEntity(Member member, Allergy allergy) {
+        return MemberAllergy.builder()
+                .id(this.id)
+                .member(member)
+                .allergy(allergy)
+                .build();
+
+    }
+}
