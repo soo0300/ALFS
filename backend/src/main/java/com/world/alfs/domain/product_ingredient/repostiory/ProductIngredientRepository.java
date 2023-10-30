57a89edbdb1e129c631e479e71df55205cb2dc83
연주원 <joo1yeon@naver.com>
2023-10-30 15:36:34 +0900

    ✨feat: 상품_원재료명 관계 repository 구현

--- backend/src/main/java/com/world/alfs/domain/product_ingredient/repostiory/ProductIngredientRepository.java
+++ backend/src/main/java/com/world/alfs/domain/product_ingredient/repostiory/ProductIngredientRepository.java
@@ -1,10 +1,16 @@
 package com.world.alfs.domain.product_ingredient.repostiory;
 
+import com.world.alfs.domain.ingredient.Ingredient;
+import com.world.alfs.domain.product.Product;
 import com.world.alfs.domain.product_ingredient.ProductIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductIngredientRepository extends JpaRepository<ProductIngredient, Long> {
+
+    List<ProductIngredient> findByIngredient_Id(Long id);
 
 }
