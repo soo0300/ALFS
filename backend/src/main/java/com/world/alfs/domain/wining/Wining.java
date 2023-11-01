045539cd8e43c5136b7ac7b961d470a26cf7d125
손효민 <sondoh100@naver.com>
2023-11-01 14:48:08 +0900

    ✨feat: 특가당첨자 엔티티 추가

--- backend/src/main/java/com/world/alfs/domain/wining/Wining.java
+++ backend/src/main/java/com/world/alfs/domain/wining/Wining.java
@@ -1,5 +1,7 @@
 package com.world.alfs.domain.wining;
 
+import com.world.alfs.domain.member.Member;
+import com.world.alfs.domain.special.Special;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,15 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Wining {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Special special;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 
 }
