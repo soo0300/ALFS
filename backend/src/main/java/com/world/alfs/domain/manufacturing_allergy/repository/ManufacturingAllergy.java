e3ea28fdb6752295e4bd8a225e8488ae86df73ee
연주원 <joo1yeon@naver.com>
2023-11-01 16:24:07 +0900

    ✨feat: manufacturing_allergy entity 작성

---
+++ backend/src/main/java/com/world/alfs/domain/manufacturing_allergy/repository/ManufacturingAllergy.java
@@ -0,0 +1,36 @@
+package com.world.alfs.domain.manufacturing_allergy.repository;
+
+import com.world.alfs.domain.allergy.Allergy;
+import com.world.alfs.domain.product.Product;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ManufacturingAllergy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "allergy_id")
+    private Allergy allergy;
+
+    @Builder
+    public ManufacturingAllergy(Long id, Product product, Allergy allergy) {
+        this.id = id;
+        this.product = product;
+        this.allergy = allergy;
+    }
+
+}
