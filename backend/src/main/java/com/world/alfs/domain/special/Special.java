2b0ba4a896074e4e8e6ac6c20a42738e6954180e
손효민 <sondoh100@naver.com>
2023-11-03 13:47:29 +0900

    💿backup: 특가상품등록 도메인 백업

--- backend/src/main/java/com/world/alfs/domain/special/Special.java
+++ backend/src/main/java/com/world/alfs/domain/special/Special.java
@@ -4,10 +4,12 @@ import com.world.alfs.domain.TimeBaseEntity;
 import com.world.alfs.domain.product.Product;
 import com.world.alfs.domain.supervisor.Supervisor;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -30,8 +32,25 @@ public class Special extends TimeBaseEntity {
     @Column
     private int salePrice;
 
+    @Column
+    private LocalDateTime start;
+
+    @Column
+    private LocalDateTime end;
+
     @ManyToOne
     @JoinColumn(name = "supervisor_id")
     private Supervisor supervisor;
 
+    @Builder
+    public Special(Long id, Product product, int status, int count, int salePrice, Supervisor supervisor, LocalDateTime start, LocalDateTime end) {
+        this.id = id;
+        this.product = product;
+        this.status = status;
+        this.count = count;
+        this.salePrice = salePrice;
+        this.supervisor = supervisor;
+        this.start = start;
+        this.end = end;
+    }
 }
