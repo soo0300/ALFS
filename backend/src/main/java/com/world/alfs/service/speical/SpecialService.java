42980328ba0a58dac25f4a4732e16e303ba418e0
손효민 <sondoh100@naver.com>
2023-11-03 13:47:10 +0900

    💿backup: 특가상품등록 서비스 백업

--- backend/src/main/java/com/world/alfs/service/speical/SpecialService.java
+++ backend/src/main/java/com/world/alfs/service/speical/SpecialService.java
@@ -1,9 +1,16 @@
 package com.world.alfs.service.speical;
 
+import com.world.alfs.domain.product.Product;
+import com.world.alfs.domain.product.repository.ProductRepository;
 import com.world.alfs.domain.special.repository.SpecialRepository;
+import com.world.alfs.domain.supervisor.Supervisor;
+import com.world.alfs.domain.supervisor.repository.SupervisorRepository;
+import com.world.alfs.service.speical.dto.AddSpecialDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,6 +18,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class SpecialService {
 
     private final SpecialRepository specialRepository;
+    private final ProductRepository productRepository;
+    private final SupervisorRepository supervisorRepository;
+
+    public Long addSpecial(AddSpecialDto dto) {
+        Optional<Product> product = productRepository.findById(dto.getProductId());
+        Optional<Supervisor> supervisor = supervisorRepository.findById(dto.getSupervisorId());
+
+//        AddSpecialDto
+
+        return null;
+    }
+
+//    public List<> getAllProduct(){
+//
+//    }
+//
+//    public Long getProduct(Long id){
+//
+//    }
+//
+//    public Long setProduct(Long id, AddSpecialDto dto){
+//
+//    }
+//
+//    public Long deleteProduct(Long id){
+//
+//    }
 
 
 }
