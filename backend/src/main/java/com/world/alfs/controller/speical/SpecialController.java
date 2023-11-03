9772ebc3e0255fd21c6459e82e58677c724e8cb6
손효민 <sondoh100@naver.com>
2023-11-03 13:46:54 +0900

    💿backup: 특가상품등록 컨트롤러 백업

--- backend/src/main/java/com/world/alfs/controller/speical/SpecialController.java
+++ backend/src/main/java/com/world/alfs/controller/speical/SpecialController.java
@@ -1,12 +1,11 @@
 package com.world.alfs.controller.speical;
 
+import com.world.alfs.controller.speical.request.AddSpecialReqeust;
 import com.world.alfs.service.speical.SpecialService;
+import com.world.alfs.service.speical.dto.AddSpecialDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +16,36 @@ public class SpecialController {
     private final SpecialService specialService;
 
     @PostMapping()
-    public Long addSpecial(@RequestBody AddSpecialRequest reqest){
-
+    public Long addSpecial(@RequestBody AddSpecialReqeust request){
+        AddSpecialDto dto = request.toDto();
+        return specialService.addSpecial(dto);
     }
+
+//    @GetMapping("/all")
+//    public ApiResponse<List<GetSpecialListResponse>> getAllProduct(){
+//        List<GetSpecialListResponse> specialList = specialService.getAllProduct();
+//        return ApiResponse.ok(specialList);
+//    }
+//
+//
+//    @GetMapping("/{id}")
+//    public ApiResponse<SpecialResponse> getProduct(@PathVariable Long id){
+//        Optional<SpecialResponse> saveSpecial =  specialService.getProduct(id);
+//        return ApiResponse.ok(saveSpecial);
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public ApiResponse<Long> setProduct(@PathVariable Long id, @RequestBody AddSpecialReqeust request){
+//        AddSpecialDto dto = request.toDto();
+//        Long saveId = specialService.setProduct(id, dto);
+//        return ApiResponse.ok(saveId);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ApiResponse<Long> deleteProduct(@PathVariable Long id){
+//        Long deleteId = specialService.deleteProduct(id);
+//        return ApiResponse.ok(deleteId);
+//    }
+
+
 }
