3dc45b7fc21a1ade4d3ce5422f48750b4668e19e
daehyuk <sdh5645@gmail.com>
2023-11-06 16:46:31 +0900

    🚨fix: 로그인수정

--- frontend/src/app/api/auth/[...nextauth]/route.tsx
+++ frontend/src/app/api/auth/[...nextauth]/route.tsx
@@ -1,47 +1,5 @@
-import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/lib/auth";
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        identifier: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-        userId: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: any) {
-        console.log(credentials);
-        console.log("들어와지긴함");
-        const res = await fetch("https://k9c204.p.ssafy.io/api/member/login", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
-        console.log(user);
-        console.log(process.env.NEXTAUTH_URL);
-        user.name = credentials.userId;
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user;
-        }
-        return null;
-      },
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }: any) {
-      // Send properties to the client, like an access_token and user id from a provider.
-
-      return session;
-    },
-  },
-  pages: {
-    // error: "/main",
-    // signIn: "/login",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
