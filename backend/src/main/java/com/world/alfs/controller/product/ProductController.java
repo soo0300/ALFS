d9695ad15ca98e1b5256fb10c44cc8c1a1f6f850
김수진 <soo0300@naver.com>
2023-11-06 17:52:52 +0900

    ✨feat: allergy filter 오류 방지

--- backend/src/main/java/com/world/alfs/controller/product/ProductController.java
+++ backend/src/main/java/com/world/alfs/controller/product/ProductController.java
@@ -4,6 +4,7 @@ import com.world.alfs.controller.ApiResponse;
 import com.world.alfs.controller.product.request.AddProductRequest;
 import com.world.alfs.controller.product.response.GetProductListResponse;
 import com.world.alfs.controller.product.response.ProductResponse;
+import com.world.alfs.domain.ingredient.Ingredient;
 import com.world.alfs.service.Ingredient_allergy.IngredientAllergyService;
 import com.world.alfs.service.allergy.AllergyService;
 import com.world.alfs.service.member_allergy.MemberAllergyService;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,27 +47,54 @@ public class ProductController {
     public ApiResponse<List<GetProductListResponse>> getAllProduct(@PathVariable Long memberId) {
         List<Long> product_list = productService.getAllProductId();
         List<GetProductListResponse> response = productService.getAllProduct();
-        for (int i = 0; i < product_list.size(); i++) {
+
+        /*
+        for (int i = 0; i < 1; i++) {
+//        for (int i = 0; i < product_list.size(); i++) {
             //step 1. product_id로 product_ingredient 로 [ingredient] 를 조회하여 ingredient_id를 반환한다.
-            List<Long> ingredient_list = productIngredientService.getAllIngredientId(product_list.get(i));
-            System.out.println("step1 pass");
+            List<Ingredient> ingredient_list = productIngredientService.getAllIngredientId(product_list.get(i));
+            System.out.println("step1 pass "+ingredient_list.size());
+            for(int a=0; a<ingredient_list.size(); a++){
+                System.out.print(ingredient_list.get(a).getId()+" ");
+            }
+            System.out.println();
 
             //step 2. [ingredient] 로 [ingredient_allergy]의 allergy_id를 조회한다.
-            List<Long> allergy_list = ingredientAllergyService.getAllAllergyId(ingredient_list);
-            System.out.println("step2 pass");
+
+            List<Long> allergy_list = new ArrayList<>();
+            System.out.println("step2 pass ");
+            for(int a=0; a<ingredient_list.size(); a++){
+                ingredientAllergyService.getAllAllergyId(allergy_list, ingredient_list.get(a));
+            }
+            for(int a=0; a<allergy_list.size(); a++){
+                System.out.print(allergy_list.get(a)+" ");
+            }
+            System.out.println();
 
             //step 3. allergy_id와 동일하고 member_id가 같은 컬럼을 [member_allergy]에서 조회한다.
+            //member_allergy_list 에 아무것도 저장안된!!!!!!
             List<Long> member_allergy_list = memberAllergyService.getFilteredAllergyId(memberId, allergy_list);
-            System.out.println("step3 pass");
+            System.out.println("step3 pass "+member_allergy_list.size());
+            for(int a=0; a<allergy_list.size(); a++){
+                System.out.print(allergy_list.get(a)+" ");
+            }
+            System.out.println();
 
             //step 3. 여기서 조회된(필터된) allergy_id를 가지고 [allergy]의 allergy_type 을 반환한다.
             List<Integer> FilterCode = allergyService.getAllergyType(member_allergy_list);
             System.out.println("step4 pass");
+//            for(int a=0; a<member_allergy_list.size(); a++){
+//                System.out.print(member_allergy_list.get(a));
+//            }
+            System.out.println();
 
             //step 4. response 에 allergy_type 을 세팅한다.
             response.get(i).setCode(FilterCode);
             System.out.println("step5 pass");
         }
+        */
+
+
         return ApiResponse.ok(response);
     }
 
