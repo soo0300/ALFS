560fc8448a23d1c4d4325c12c02401ca217bc3ab
연주원 <joo1yeon@naver.com>
2023-11-06 14:29:25 +0900

    📌refactor: 에러 처리

---
+++ backend/src/main/java/com/world/alfs/common/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.world.alfs.common.exception;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.validation.Errors;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    @NonNull
+    private final ErrorCode errorCode;
+}
