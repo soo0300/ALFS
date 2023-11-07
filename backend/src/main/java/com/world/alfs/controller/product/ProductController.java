6a5314ce7f40a95fc12cd7df469dd3c78d50fd2a
김수진 <soo0300@naver.com>
2023-11-07 15:39:56 +0900

    📝docs: 불필요한 주석 제거

--- backend/src/main/java/com/world/alfs/controller/product/ProductController.java
+++ backend/src/main/java/com/world/alfs/controller/product/ProductController.java
@@ -48,48 +48,25 @@ public class ProductController {
     @GetMapping("/all/{memberId}")
     public ApiResponse<List<GetProductListResponse>> getAllProduct(@PathVariable Long memberId) {
         List<Long> product_list = productService.getAllProductId();
-        List<String> ingredientName = new ArrayList<>();
         List<GetProductListResponse> response = productService.getAllProduct();
-
-        // allergy 테이블에서 member_allergy_list 에 해당하는 allergy_name과
-        // ingredient 테이블에서 product_ingredient_list 에 해당하는 name와 일치한다면,
-        // allergy 테이블에서 allergy_id를 저장한 후, allergy_type 를 가졍한다.
-        // 여기서 조회된(필터된) allergy_id를 가지고 [allergy]의 allergy_type 을 반환한다.
-
         for (int i = 0; i < product_list.size(); i++) {
-            //step 1. product_id로 product_ingredient 로 [ingredient] 를 조회하여 ingredient_id를 반환한다.
             List<String> compare_ingredient = new ArrayList<>();
             List<Ingredient> product_ingredient_list = productIngredientService.getAllIngredientId(product_list.get(i));
-            System.out.println("step1 pass ");
             for (int a = 0; a < product_ingredient_list.size(); a++) {
                 compare_ingredient.add(product_ingredient_list.get(a).getName());
-                System.out.print(product_ingredient_list.get(a).getId() + " ");
             }
-            System.out.println();
-
             List<Long> memberAllergy_allergy_id_list = memberAllergyService.getFilteredAllergyId(memberId);
-            System.out.println("step2 pass ");
-            for (int a = 0; a < memberAllergy_allergy_id_list.size(); a++) {
-                System.out.print(memberAllergy_allergy_id_list.get(a) + " ");
-            }
-            System.out.println();
-
             List<Integer> FilterCode = new ArrayList<>();
-
-            System.out.println("step3 pass ");
             for (int a = 0; a < memberAllergy_allergy_id_list.size(); a++) {
                 Allergy allergy = allergyService.getAllergy(memberAllergy_allergy_id_list.get(a));
                 for (int b = 0; b < compare_ingredient.size(); b++) {
                     if (compare_ingredient.get(b).equals(allergy.getAllergyName())) {
-                        System.out.print(compare_ingredient.get(b) + " ");
                         FilterCode.add(allergy.getAllergyType());
                     }
                 }
             }
             response.get(i).setCode(FilterCode);
         }
-
-
         return ApiResponse.ok(response);
     }
 
