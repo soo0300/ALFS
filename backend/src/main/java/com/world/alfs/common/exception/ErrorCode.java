484c78846c35110f9679ea636f3abe73206aa414
연주원 <joo1yeon@naver.com>
2023-11-07 10:11:35 +0900

    ✨feat: 원재료 관련 에러 코드 추가

--- backend/src/main/java/com/world/alfs/common/exception/ErrorCode.java
+++ backend/src/main/java/com/world/alfs/common/exception/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     /* 403 FORBIDDEN : 페이지 접근 거부 */
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
-    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다.");
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
+    INGREDIENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 원재료가 존재하지 않습니다.");
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
 
