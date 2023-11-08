a347f8093232dd0445661bcdabc702a509a42d41
연주원 <joo1yeon@naver.com>
2023-11-08 12:13:04 +0900

    ✨feat: 제조시설 등록 controller 작성

--- backend/src/main/java/com/world/alfs/controller/manufacturing_allergy/ManufacturingAllergyController.java
+++ backend/src/main/java/com/world/alfs/controller/manufacturing_allergy/ManufacturingAllergyController.java
@@ -1,7 +1,9 @@
 package com.world.alfs.controller.manufacturing_allergy;
 
+import com.world.alfs.controller.manufacturing_allergy.request.AddManuAllergyRequest;
 import com.world.alfs.controller.manufacturing_allergy.request.GetManuAllergyRequest;
 import com.world.alfs.service.manufacturing_allergy.ManufacturingAllergyService;
+import com.world.alfs.service.manufacturing_allergy.dto.AddManuAllergyDto;
 import com.world.alfs.service.manufacturing_allergy.dto.GetManuAllergyDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +28,14 @@ public class ManufacturingAllergyController {
         boolean isManuAllergy = manufacturingAllergyService.getManuAllergy(dto);
 
         return isManuAllergy;
+    }
+
+    @PostMapping("/add")
+    public Long addManuAllergy(@RequestBody AddManuAllergyRequest request) {
+        List<AddManuAllergyDto> dtoList = request.toDto();
+        Long productId = manufacturingAllergyService.addManuAllergy(request.getProductId(), dtoList);
+
+        return productId;
     }
 
 }
