dae7423824dfb3efa3b2a55a1d754d86fa08c362
연주원 <joo1yeon@naver.com>
2023-11-08 12:13:34 +0900

    ✨feat: 제조시설 등록 service 작성

--- backend/src/main/java/com/world/alfs/service/manufacturing_allergy/ManufacturingAllergyService.java
+++ backend/src/main/java/com/world/alfs/service/manufacturing_allergy/ManufacturingAllergyService.java
@@ -1,8 +1,16 @@
 package com.world.alfs.service.manufacturing_allergy;
 
+import com.world.alfs.common.exception.CustomException;
+import com.world.alfs.common.exception.ErrorCode;
+import com.world.alfs.domain.allergy.Allergy;
+import com.world.alfs.domain.allergy.repository.AllergyRepository;
+import com.world.alfs.domain.manufacturing_allergy.ManufacturingAllergy;
 import com.world.alfs.domain.manufacturing_allergy.repository.ManufacturingAllergyRepository;
 import com.world.alfs.domain.member_allergy.MemberAllergy;
 import com.world.alfs.domain.member_allergy.repository.MemberAllergyRepository;
+import com.world.alfs.domain.product.Product;
+import com.world.alfs.domain.product.repository.ProductRepository;
+import com.world.alfs.service.manufacturing_allergy.dto.AddManuAllergyDto;
 import com.world.alfs.service.manufacturing_allergy.dto.GetManuAllergyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.world.alfs.common.exception.ErrorCode.ALLERGY_NOT_FOUND;
+import static com.world.alfs.common.exception.ErrorCode.PRODUCT_NOT_FOUND;
+
 @Transactional
 @Service
 @RequiredArgsConstructor
@@ -18,7 +29,10 @@ public class ManufacturingAllergyService {
 
     private final ManufacturingAllergyRepository manufacturingAllergyRepository;
     private final MemberAllergyRepository memberAllergyRepository;
+    private final ProductRepository productRepository;
+    private final AllergyRepository allergyRepository;
 
+    // 제조시설 알러지 조회
     public boolean getManuAllergy(GetManuAllergyDto dto) {
         // 회원 알러지 찾기
         List<MemberAllergy> memberAllergyList = memberAllergyRepository.findByMemberId(dto.getMemberId());
@@ -33,6 +47,30 @@ public class ManufacturingAllergyService {
         int cnt = manufacturingAllergyRepository.findCountByProductAndAllergy(dto.getProductId(), allergyList);
 
         return cnt > 0;
+    }
+
+    // 제조시설 등록
+    public Long addManuAllergy(Long productId, List<AddManuAllergyDto> dtoList) {
+        List<ManufacturingAllergy>  manufacturingAllergyList = new ArrayList<>();
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() ->  new CustomException(PRODUCT_NOT_FOUND));
+
+        for (AddManuAllergyDto addManuAllergyDto : dtoList) {
+            Allergy allergy = allergyRepository.findByAllergyName(addManuAllergyDto.getAllergyName());
+                    //.orElseThrow(() -> new CustomException(ALLERGY_NOT_FOUND));
+
+            ManufacturingAllergy manufacturingAllergy = ManufacturingAllergy.builder()
+                    .product(product)
+                    .allergy(allergy)
+                    .build();
+
+            manufacturingAllergyList.add(manufacturingAllergy);
+        }
+
+        manufacturingAllergyRepository.saveAll(manufacturingAllergyList);
+
+        return productId;
     }
 
 }
