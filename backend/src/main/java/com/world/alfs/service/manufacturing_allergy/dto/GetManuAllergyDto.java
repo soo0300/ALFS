0fa4eeee0f6208a8fb6dafdabc5800c0a4a4277a
연주원 <joo1yeon@naver.com>
2023-11-08 12:12:04 +0900

    📌refactor: @Build 관련 불필요한 코드 삭제

--- backend/src/main/java/com/world/alfs/service/manufacturing_allergy/dto/GetManuAllergyDto.java
+++ backend/src/main/java/com/world/alfs/service/manufacturing_allergy/dto/GetManuAllergyDto.java
@@ -1,20 +1,14 @@
 package com.world.alfs.service.manufacturing_allergy.dto;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Data
-@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetManuAllergyDto {
 
     private Long memberId;
     private Long productId;
-
-    @Builder
-    public GetManuAllergyDto(Long memberId, Long productId) {
-        this.memberId = memberId;
-        this.productId = productId;
-    }
     
 }
