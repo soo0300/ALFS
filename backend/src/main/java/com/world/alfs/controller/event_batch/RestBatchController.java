8649c932078437441beb2a126abf794d1efd5d23
손효민 <sondoh100@naver.com>
2023-11-09 09:25:12 +0900

    🌈style: 스프링 배치 테스트 컨트롤러단 주석

--- backend/src/main/java/com/world/alfs/controller/event_batch/RestBatchController.java
+++ backend/src/main/java/com/world/alfs/controller/event_batch/RestBatchController.java
@@ -24,22 +24,22 @@ public class RestBatchController {
     private final JobLauncher jobLauncher;
     private final JobRegistry jobRegistry;
     private final JobExplorer jobExplorer;
-
-    @PostMapping("/test")
-    public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws Exception {
-        // 잡 이름의 빈 검색
-        Job job = jobRegistry.getJob(request.getName());
-
-        // job 파라미터 추출
-        // builder의 인자 -> (현재 만드는 잡 파라미터에 추가하고 싶은 잡파라미터, jobExplorer)
-        // jobExplorer가 필요한 이유는 다음 파라미터를 위해 기존 run.id를 가져오기 위해서 필요함
-        JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(),jobExplorer)
-                .getNextJobParameters(job)
-                .toJobParameters();
-
-        // 잡 실행하고 ExitStatus 반환
-        return this.jobLauncher.run(job, jobParameters).getExitStatus();
-//        return this.jobLauncher.run(job).getExitStatus();
-    }
+//
+//    @PostMapping("/test")
+//    public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws Exception {
+//        // 잡 이름의 빈 검색
+//        Job job = jobRegistry.getJob(request.getName());
+//
+//        // job 파라미터 추출
+//        // builder의 인자 -> (현재 만드는 잡 파라미터에 추가하고 싶은 잡파라미터, jobExplorer)
+//        // jobExplorer가 필요한 이유는 다음 파라미터를 위해 기존 run.id를 가져오기 위해서 필요함
+//        JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(),jobExplorer)
+//                .getNextJobParameters(job)
+//                .toJobParameters();
+//
+//        // 잡 실행하고 ExitStatus 반환
+//        return this.jobLauncher.run(job, jobParameters).getExitStatus();
+////        return this.jobLauncher.run(job).getExitStatus();
+//    }
 
 }
