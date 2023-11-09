6234a25f882a42173f1cc6043822a552513217a4
손효민 <sondoh100@naver.com>
2023-11-09 09:24:01 +0900

    🌈style: 주석 제거

--- backend/src/test/java/com/world/alfs/config/batch/BatchJobConfigurationTest.java
+++ backend/src/test/java/com/world/alfs/config/batch/BatchJobConfigurationTest.java
@@ -1,10 +1,8 @@
 package com.world.alfs.config.batch;
 
-import com.world.alfs.AlfsApplication;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.test.JobLauncherTestUtils;
@@ -12,13 +10,10 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-//@TestPropertySource(locations = {"classpath:/application.properties","classpath:/application-ignore.properties"})
 @SpringBatchTest
 @EnableBatchProcessing
 @ExtendWith(SpringExtension.class)
@@ -36,8 +31,6 @@ public class BatchJobConfigurationTest {
     @Test
     @DisplayName("simple job success")
     void simpleJob_success() throws Exception{
-        // Configure the behavior of the mock Job and Step beans
-//        when(job.execute(any())).thenReturn(new JobExecution(0L));
 
         // given
 
