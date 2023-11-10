945a95b62d03b9ef98a5181d729e3b5edaddd4ee
김수진 <soo0300@naver.com>
2023-11-10 21:28:54 +0900

    ✨feat: 더미 INSERT문

---
+++ exec/DB/231110/special.sql
@@ -0,0 +1,5 @@
+INSERT INTO alfs.special (count, end, sale_price, start, status, product_id, supervisor_id) VALUES (10, '2023-12-07 23:43:00.000000', 3000, '2023-11-14 23:42:00.000000', 0, 1, 1);
+INSERT INTO alfs.special (count, end, sale_price, start, status, product_id, supervisor_id) VALUES (10, '2023-12-06 02:43:00.000000', 1000, '2023-11-11 02:42:00.000000', 0, 4, 1);
+INSERT INTO alfs.special (count, end, sale_price, start, status, product_id, supervisor_id) VALUES (10, '2023-11-14 23:43:00.000000', 1000, '2023-11-06 23:42:00.000000', 1, 11, 2);
+INSERT INTO alfs.special (count, end, sale_price, start, status, product_id, supervisor_id) VALUES (3, '2023-11-12 09:00:02.000000', 500, '2023-11-10 16:24:00.000000', 1, 15, 3);
+INSERT INTO alfs.special (count, end, sale_price, start, status, product_id, supervisor_id) VALUES (3, '2023-11-11 16:29:40.000000', 500, '2023-11-10 21:20:00.000000', 0, 16, 3);
