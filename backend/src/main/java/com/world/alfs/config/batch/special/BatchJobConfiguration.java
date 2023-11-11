c8b66336845a27a079c2b518978e1492df0041a2
손효민 <sondoh100@naver.com>
2023-11-11 16:49:19 +0900

    ✨feat: 특가상품 스케쥴러 적용 후 jobParameter 이용해 batch로 이벤트 상태 진행중으로 변경

--- backend/src/main/java/com/world/alfs/config/batch/special/BatchJobConfiguration.java
+++ backend/src/main/java/com/world/alfs/config/batch/special/BatchJobConfiguration.java
@@ -4,14 +4,13 @@ import com.world.alfs.domain.special.Special;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,15 +37,17 @@ public class BatchJobConfiguration {
 
     @Bean
     public Job specialStartJob(){
+        // log.info("specialStartJob");
         return jobBuilderFactory.get(JOB_NAME)
                 .start(specialStartStep())
                 .incrementer(new RunIdIncrementer()) // listener 추가 가능
-                .listener(jobCompletionNotificationListener)
+//                .listener(jobCompletionNotificationListener)
                 .build();
     }
 
     @Bean
     public Step specialStartStep(){
+        // log.info("specialStartStep");
         return stepBuilderFactory.get(STEP_NAME)
                 .<Special, Special>chunk(CHUNK_SIZE)
                 .reader(customItemReader(0L, 0L))
@@ -56,38 +57,45 @@ public class BatchJobConfiguration {
     }
 
     @Bean
-    public ItemReader<Special> customItemReader(@Value("#{jobParameters['supervisorId']}") Long supervisorId, @Value("#{jobParameters['productId']}") Long productId) {
+    @StepScope
+    public JpaCursorItemReader<Special> customItemReader(@Value("#{jobParameters['supervisorId']}") Long supervisorId, @Value("#{jobParameters['productId']}") Long productId) {
+        // log.info("customItemReader");
+//        log.debug("jobParameter 확인: "+supervisorId+" "+productId);
+        if (supervisorId.equals(0L) || productId.equals(0L)) {
+            throw new IllegalArgumentException("supervisorId와 productId는 필수 파라미터입니다.");
+        }
+
         HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("id", supervisorId);
+        parameters.put("supervisorId", supervisorId);
         parameters.put("productId", productId);
         parameters.put("status",0);
+
+        String queryString = "select s from Special s where s.supervisor.id = :supervisorId and s.status = :status and s.product.id = :productId";
+//        log.debug("쿼리 확인: " + queryString);
 
         return new JpaCursorItemReaderBuilder<Special>()
                 .name("jpaCursorItemReader")
                 .entityManagerFactory(entityManagerFactory)
-                .queryString("select s from Special s where supervisor_id =:id and status =:status and product_id =: productId")
+                .queryString(queryString)
                 .parameterValues(parameters)
                 .build();
     }
 
     @Bean
+    @StepScope
     public ItemProcessor<Special, Special> customItemProcessor() {
-        return new CustomItemProcessor();
-    }
-
-    public static class CustomItemProcessor implements ItemProcessor<Special, Special> {
-        @Override
-        public Special process(Special item) {
-            item.setStatus(1); // Special 객체의 status를 0에서 1로 변경
-            return item;
-        }
+        // log.info("customItemProcessor");
+        return new CustomJpaItemProcessor(entityManagerFactory);
     }
 
     @Bean
+    @StepScope
     @Transactional
-    public ItemWriter<Special> customItemWriter() {
+    public JpaItemWriter<Special> customItemWriter() {
+        // log.info("customItemWriter");
         JpaItemWriter<Special> itemWriter = new JpaItemWriter<>();
         itemWriter.setEntityManagerFactory(entityManagerFactory);
         return itemWriter;
     }
+
 }
