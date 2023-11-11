939e4cfee27ca53556afc993d7aab8ed56603601
손효민 <sondoh100@naver.com>
2023-11-11 16:40:58 +0900

    ✨feat: 스케쥴링 어노테이션 추가

--- backend/src/main/java/com/world/alfs/AlfsApplication.java
+++ backend/src/main/java/com/world/alfs/AlfsApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableScheduling
+@EnableScheduling
 @EnableBatchProcessing
 @SpringBootApplication(
 		exclude = {
-			org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-			org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-			org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-})
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		})
 public class AlfsApplication {
 
 	public static void main(String[] args) {
