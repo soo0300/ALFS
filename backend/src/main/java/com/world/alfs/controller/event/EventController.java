b8ed2e439daf68def8f6aae0a9d7cf2f39c6e002
김수진 <soo0300@naver.com>
2023-11-12 19:13:12 +0900

    ✨feat: event 선택 컨트롤러 작성

--- backend/src/main/java/com/world/alfs/controller/event/EventController.java
+++ backend/src/main/java/com/world/alfs/controller/event/EventController.java
@@ -1,7 +1,9 @@
 package com.world.alfs.controller.event;
 
 import com.world.alfs.controller.ApiResponse;
+import com.world.alfs.controller.event.request.EventChooseRequest;
 import com.world.alfs.service.event.EventService;
+import com.world.alfs.service.event.dto.EventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -21,4 +23,10 @@ public class EventController {
 
     //사용자가 event를 눌렀을 떄, 해당 이벤트의 결과를 반환해줘야 한다.
     //이벤트 결과를 redis에 저장하고 있기. 그 값을 다시 DB에 저장
+    @PostMapping()
+    ApiResponse<Long> choose(EventChooseRequest request){
+        EventDto dto = request.toDto();
+        //eventService.choose(dto)
+        return ApiResponse.ok(1L);
+    }
 }
