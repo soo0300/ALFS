6443e79a79ce5216c4de597bafd470bb18fbd4e4
손효민 <sondoh100@naver.com>
2023-11-12 14:43:48 +0900

    ✨feat: 특가상품 종료 스케쥴러

--- backend/src/main/java/com/world/alfs/config/batch/special/BatchScheduler.java
+++ backend/src/main/java/com/world/alfs/config/batch/special/BatchScheduler.java
@@ -40,7 +40,7 @@ public class BatchScheduler {
 
         LocalDateTime parsedDateTime = LocalDateTime.parse(parsedTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         List<Special> specials = specialRepository.findByStart(parsedDateTime);
-        log.info("specials = {}", specials.toString());
+//        log.info("specials = {}", specials.toString());
 
         for (Special special : specials) {
             try {
@@ -51,6 +51,50 @@ public class BatchScheduler {
                         .toJobParameters();
 
                 JobExecution jobExecution = jobLauncher.run(batchJobConfiguration.specialStartJob(), jobParameters);
+
+                // 여기서 JobExecution 결과를 처리하거나 로깅할 수 있습니다.
+                if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+                    // 성공적으로 완료된 경우의 로직
+                    log.debug("Job Execution: " + jobExecution.getStatus());
+                    log.debug("Job getJobConfigurationName: " + jobExecution.getJobConfigurationName());
+                    log.debug("Job getJobId: " + jobExecution.getJobId());
+                    log.debug("Job getExitStatus: " + jobExecution.getExitStatus());
+                    log.debug("Job getJobInstance: " + jobExecution.getJobInstance());
+                    log.debug("Job getStepExecutions: " + jobExecution.getStepExecutions());
+                    log.debug("Job getLastUpdated: " + jobExecution.getLastUpdated());
+                    log.debug("Job getFailureExceptions: " + jobExecution.getFailureExceptions());
+                } else if (jobExecution.getStatus() == BatchStatus.FAILED) {
+                    // 실패한 경우의 로직
+                    log.debug("jobExecution 이 실패한 경우");
+                }
+            } catch (Exception e) {
+                // 예외 처리 로직
+                log.debug("jobExecution 이 실패했습니다");
+                log.error(e.getMessage());
+            }
+
+        }
+    }
+
+    @Scheduled(fixedRate = 1000) // 1초마다
+    public void runSpecialEndJob(){
+        LocalDateTime currentDateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        String parsedTime = currentDateTime.format(DateTimeFormatter.ofPattern( "yyyy-MM-dd HH:mm:ss"));
+//        log.debug("현시간:"+parsedTime);
+
+        LocalDateTime parsedDateTime = LocalDateTime.parse(parsedTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        List<Special> specials = specialRepository.findByEnd(parsedDateTime);
+//        log.info("specials = {}", specials.toString());
+
+        for (Special special : specials) {
+            try {
+                JobParameters jobParameters = new JobParametersBuilder()
+                        .addLong("currentTime", System.currentTimeMillis())
+                        .addLong("supervisorId", special.getSupervisor().getId())
+                        .addLong("productId", special.getProduct().getId())
+                        .toJobParameters();
+
+                JobExecution jobExecution = jobLauncher.run(batchJobConfiguration.specialEndJob(), jobParameters);
 
                 // 여기서 JobExecution 결과를 처리하거나 로깅할 수 있습니다.
                 if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
