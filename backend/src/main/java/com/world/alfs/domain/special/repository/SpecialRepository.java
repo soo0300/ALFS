29c3dac4eb47e3718ff43bf642355028039ba020
손효민 <sondoh100@naver.com>
2023-11-13 12:01:40 +0900

    ✨feat: 특가상품 이벤트 상품 조회 JPA 함수

--- backend/src/main/java/com/world/alfs/domain/special/repository/SpecialRepository.java
+++ backend/src/main/java/com/world/alfs/domain/special/repository/SpecialRepository.java
@@ -2,6 +2,7 @@ package com.world.alfs.domain.special.repository;
 
 import com.world.alfs.domain.special.Special;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,4 +14,7 @@ public interface SpecialRepository extends JpaRepository<Special, Long> {
         List<Special> findByStart(LocalDateTime time);
 
         List<Special> findByEnd(LocalDateTime time);
+
+        @Query("SELECT s.status FROM Special s WHERE s.product.id =:id")
+        int findByStatus(Long id);
 }
