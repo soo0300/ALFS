3e244a5cde446951c0367caca38259dd0ceca23a
안종상 <skqlck-@naver.com>
2023-11-13 14:11:27 +0900

    ⚡patch: 장바구니 알러지 필터링 추가

--- frontend/src/app/api/bigsalelist/BigSaleList.tsx
+++ frontend/src/app/api/bigsalelist/BigSaleList.tsx
@@ -8,3 +8,12 @@ export async function GetBigSaleList() {
     console.error(e);
   }
 }
+
+export async function GetBigSaleFilteredList(member_id : string){
+  try {
+    const res = await baseAxios.get(`api/special/all/${member_id}`);
+    return res.data.data;
+  } catch (e) {
+    console.error(e);
+  }
+}
