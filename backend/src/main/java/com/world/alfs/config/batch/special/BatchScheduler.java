cc88c0d199f2de3aed49b9ab187e30d566237ef6
김수진 <soo0300@naver.com>
2023-11-15 14:26:48 +0900

    ✨feat: 이벤트 스케줄링 작성

--- backend/src/main/java/com/world/alfs/config/batch/special/BatchScheduler.java
+++ backend/src/main/java/com/world/alfs/config/batch/special/BatchScheduler.java
@@ -4,7 +4,6 @@ import com.world.alfs.domain.event.Event;
 import com.world.alfs.domain.event.repository.EventRepository;
 import com.world.alfs.domain.special.Special;
 import com.world.alfs.domain.special.repository.SpecialRepository;
-import com.world.alfs.service.event.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
@@ -20,7 +19,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Component
@@ -47,7 +45,6 @@ public class BatchScheduler {
         List<Special> specials = specialRepository.findByStart(parsedDateTime);
 
 
-
         for (Event event : events) {
             try {
                 JobParameters jobParameters = new JobParametersBuilder()
@@ -56,7 +53,7 @@ public class BatchScheduler {
                         .addLong("eventId", event.getId())
                         .toJobParameters();
 
-                JobExecution jobExecution = jobLauncher.run(batchJobConfiguration.specialStartJob(), jobParameters);
+                JobExecution jobExecution = jobLauncher.run(batchJobConfiguration.specialStartJob2(), jobParameters);
 
                 // 여기서 JobExecution 결과를 처리하거나 로깅할 수 있습니다.
                 if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
@@ -111,9 +108,7 @@ public class BatchScheduler {
                 log.debug("jobExecution 이 실패했습니다");
                 log.error(e.getMessage());
             }
-
         }
-
     }
 
     @Scheduled(fixedRate = 1000) // 1초마다
@@ -124,7 +119,42 @@ public class BatchScheduler {
 
         LocalDateTime parsedDateTime = LocalDateTime.parse(parsedTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         List<Special> specials = specialRepository.findByEnd(parsedDateTime);
+        List<Event> events = eventRepository.findByEnd(parsedDateTime);
 //        log.info("specials = {}", specials.toString());
+
+
+
+        for (Event event : events) {
+            try {
+                JobParameters jobParameters = new JobParametersBuilder()
+                        .addLong("currentTime", System.currentTimeMillis())
+                        .addLong("supervisorId", event.getSupervisor().getId())
+                        .addLong("eventId", event.getId())
+                        .toJobParameters();
+
+                JobExecution jobExecution = jobLauncher.run(batchJobConfiguration.specialEndJob2(), jobParameters);
+
+                // 여기서 JobExecution 결과를 처리하거나 로깅할 수 있습니다.
+                if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+                    // 성공적으로 완료된 경우의 로직
+                    log.debug("Job Execution: " + jobExecution.getStatus());
+                    log.debug("Job getJobConfigurationName: " + jobExecution.getJobConfigurationName());
+                    log.debug("Job getJobId: " + jobExecution.getJobId());
+                    log.debug("Job getExitStatus: " + jobExecution.getExitStatus());
+                    log.debug("Job getJobInstance: " + jobExecution.getJobInstance());
+                    log.debug("Job getStepExecutions: " + jobExecution.getStepExecutions());
+                    log.debug("Job getLastUpdated: " + jobExecution.getLastUpdated());
+                    log.debug("Job getFailureExceptions: " + jobExecution.getFailureExceptions());
+                } else if (jobExecution.getStatus() == BatchStatus.FAILED) {
+                    // 실패한 경우의 로직
+                    log.debug("jobExecution 이 실패한 경우");
+                }
+            } catch (Exception e) {
+                // 예외 처리 로직
+                log.debug("jobExecution 이 실패했습니다");
+                log.error(e.getMessage());
+            }
+        }
 
         for (Special special : specials) {
             try {
@@ -157,6 +187,7 @@ public class BatchScheduler {
                 log.error(e.getMessage());
             }
         }
-    }
 
+
+    }
 }
