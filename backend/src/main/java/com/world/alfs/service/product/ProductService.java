2cbaf0c7ec9ee3d2bfc3a76133ff9fc6d32c4f56
손효민 <sondoh100@naver.com>
2023-11-15 17:37:39 +0900

    ⚡patch: 상품 등록시 redis에 sale 저장

--- backend/src/main/java/com/world/alfs/service/product/ProductService.java
+++ backend/src/main/java/com/world/alfs/service/product/ProductService.java
@@ -13,29 +13,39 @@ import com.world.alfs.domain.special.repository.SpecialRepository;
 import com.world.alfs.service.product.dto.AddProductDto;
 import com.world.alfs.service.product.dto.RegisterProductDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductImgRepository productImgRepository;
     private final SpecialRepository specialRepository;
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
     public Long addProduct(RegisterProductDto dto) {
         Product product = dto.toEntity();
         ProductImg productImg = dto.toImgEntity(product);
         Product savedProduct = productRepository.save(product);
         productImgRepository.save(productImg);
+
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        Map<String, Object> map = new HashMap<>();
+        map.put(String.valueOf(product.getId()), String.valueOf(product.getSale()));
+        hashOperations.putAll("saleCache", map);
+
         return savedProduct.getId();
     }
 
